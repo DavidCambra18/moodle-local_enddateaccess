0f281bd3aa3f1f1d0e3c0d48cd8c155f2fcc34d3
DavidCambra18 <cambrafontan.david@gmail.com>
2026-06-02 13:24:11 +0200

    chore: bump version to refresh caches

--- version.php
+++ version.php
@@ -1,8 +1,15 @@
 <?php
+/**
+ * Plugin version and requirements definition.
+ *
+ * @package    local_enddateaccess
+ * @copyright  2026 David Cambra
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_enddateaccess';
-$plugin->version   = 2026052201;
+$plugin->version   = 2026060200;
 $plugin->requires  = 2018051700;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.0.0';
