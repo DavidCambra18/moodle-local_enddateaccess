70901a2404a31616084f3054ec2f6d3afbe084a2
DavidCambra18 <cambrafontan.david@gmail.com>
2026-09-14 11:39:04 +0200

    i18n: add Galician (gl) translation strings

---
+++ lang/gl/local_enddateaccess.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local enddateaccess plugin.
+ *
+ * @package    local_enddateaccess
+ * @copyright  2026 David Cambra
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['clisuccess'] = 'A sincronización manual completouse correctamente.';
+$string['enablesync'] = 'Activar a sincronización automática';
+$string['enablesync_desc'] = 'Se está activado, o complemento aplicará automaticamente restricións de data aos módulos do curso en función da data de finalización do curso.';
+$string['eventrestrictionupdated'] = 'A restrición de data do módulo actualizouse automaticamente';
+$string['pluginname'] = 'Restrición pola data de finalización do curso';
+$string['privacy:metadata'] = 'Este complemento non almacena ningún dato persoal.';
